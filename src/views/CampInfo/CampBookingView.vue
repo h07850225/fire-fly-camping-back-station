f371f48e65e0690ec71a0ff02461a586770405fb
LeonLu3303 <leon.lu1103@gmail.com>
2022-10-11 14:57:31 +0800

    ㄑasdf

--- src/views/CampInfo/CampBookingView.vue
+++ src/views/CampInfo/CampBookingView.vue
@@ -1,60 +1,34 @@
 <template>
-
-<Main></Main>
-<section class="page_section">
-
-  <header class="project_header">
-    <div class="header_left">
-      <p class="router_name">{{chtName}}</p>
-    </div>
-    <div class="header_right">
-      <div class="breadcrumb">
-        <p>{{title}} > {{chtName}}</p>
+  <Main></Main>
+  <section class="page_section">
+    <header class="project_header">
+      <div class="header_left">
+        <p class="router_name">{{ chtName }}</p>
       </div>
+      <div class="header_right">
+        <div class="breadcrumb">
+          <p>{{ title }} > {{ chtName }}</p>
+        </div>
+      </div>
+    </header>
+
+    <div class="serch_bar">
+      <input type="text" placeholder="搜尋" />
+      <button>新增</button>
+      <button>修改</button>
+      <button>刪除</button>
     </div>
-  </header>
 
-  <div class="serch_bar">
-    <input type="text" placeholder="搜尋">
-    <button>新增</button>
-    <button>修改</button>
-    <button>刪除</button>
-  </div>  
-
-  <table>
-    <tr class="table_title">
-      <th v-for="title in titles" :class="{title_list: titleList==i}">{{title.thName}}</th>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-    <tr v-for="item,index in items" :class="{item_content: itemContent==i}">
-      <td v-for="list in item.lists" :class="{list_content: listContent==i}">{{list.tdName}}</td>
-    </tr>
-  </table>
-
-</section>
-
+    <table>
+      <tr class="table_title">
+        <th v-for="item in titles" :key="item">
+          {{ item }}
+        </th>
+      </tr>
+    </table>
+  </section>
 </template>
-    
+
 <script>
 // @ is an alias to /src
 import Main from '@/components/Main.vue';
@@ -67,82 +41,25 @@ export default {
   data() {
     return {
       chtName: '營區預訂管理',
-      title:'營區預訂',
-      titles:[
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
-        {
-          thName:'test',
-        },
+      title: '營區預訂',
+      titles: [
+        '預訂號碼',
+        '會員編號',
+        '會員ID',
+        '營帳編號',
+        '營帳類型編號',
+        '付款方式',
+        '付款狀態',
+        '總金額',
+        '入住日期',
+        '退房日期',
+        '詳細資訊',
       ],
-      items:[       
-        {
-          lists:[
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-            {
-              tdName:'test',
-            },
-          ]
-        },
-      ]
-    }
+    };
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
-@import'@/assets/Scss/Page/table.scss';
-
+@import '@/assets/Scss/Page/table.scss';
 </style>
